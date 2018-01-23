a6c7b2d7ba837b2276f48f2eb9ea6bf21a8df80b
Ely Haughie <elycin2@gmail.com>
2018-01-23 07:02:11 -0500

    Adding transaction link

--- resources/views/layouts/other/transactions.blade.php
+++ resources/views/layouts/other/transactions.blade.php
@@ -10,7 +10,7 @@
 
     @foreach($transactions as $transaction)
         <tr>
-            <td>{{ $transaction->transaction_id }}</td>
+            <td><a href="/transaction/{{ $transaction->transaction_id }}">{{ $transaction->transaction_id }}</a></td>
             <td>{{ number_format($transaction->amount, 8) }}</td>
             <td>{{ $transaction->to_address }}</td>
             <td>{{ \Carbon\Carbon::parse($address->created_at)->toDayDateTimeString() }} ({{ \Carbon\Carbon::parse($address->created_at)->diffForHumans() }})</td>
