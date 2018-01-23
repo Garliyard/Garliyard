47292698e1bcf659cfcf911eb0ac1f150305b095
Ely Haughie <elycin2@gmail.com>
2018-01-23 07:35:27 -0500

    Adding google analytics

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,5 +4,13 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+
 window.$ = window.jQuery = require('jquery');
 require('./bootstrap');
+
+require('./analytics');
+// Google Analytics
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};
+ga('create', 'UA-40636205-6', 'auto');
+ga('set', 'transport', 'beacon');
+ga('send', 'pageview');
