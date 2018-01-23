80c8f18f398925353de0adbb1f1d61bd6b55a1b3
Ely Haughie <elycin2@gmail.com>
2018-01-23 10:19:19 -0500

    Making table responsive in proper div

--- resources/views/layouts/other/transactions.blade.php
+++ resources/views/layouts/other/transactions.blade.php
@@ -1,19 +1,21 @@
-<table class="table table-responsive">
-    <thead>
-    <tr>
-        <th>Transaction ID</th>
-        <th>Amount</th>
-        <th>Sent to</th>
-        <th>Creation Date</th>
-    </tr>
-    </thead>
-
-    @foreach($transactions as $transaction)
+<div class="table-responsive">
+    <table class="table">
+        <thead>
         <tr>
-            <td><a href="/transaction/{{ $transaction->transaction_id }}">{{ $transaction->transaction_id }}</a></td>
-            <td>{{ number_format($transaction->amount, 8) }}</td>
-            <td>{{ $transaction->to_address }}</td>
-            <td>{{ \Carbon\Carbon::parse($transaction->created_at)->toDayDateTimeString() }} ({{ \Carbon\Carbon::parse($transaction->created_at)->diffForHumans() }})</td>
+            <th>Transaction ID</th>
+            <th>Amount</th>
+            <th>Sent to</th>
+            <th>Creation Date</th>
         </tr>
-    @endforeach
-</table>
+        </thead>
+
+        @foreach($transactions as $transaction)
+            <tr>
+                <td><a href="/transaction/{{ $transaction->transaction_id }}">{{ $transaction->transaction_id }}</a></td>
+                <td>{{ number_format($transaction->amount, 8) }}</td>
+                <td>{{ $transaction->to_address }}</td>
+                <td>{{ \Carbon\Carbon::parse($transaction->created_at)->toDayDateTimeString() }} ({{ \Carbon\Carbon::parse($transaction->created_at)->diffForHumans() }})</td>
+            </tr>
+        @endforeach
+    </table>
+</div>
