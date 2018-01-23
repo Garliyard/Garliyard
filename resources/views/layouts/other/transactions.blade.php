c97490c3734ba48a2471a5e866e5c92d842b5717
Ely Haughie <elycin2@gmail.com>
2018-01-23 07:25:34 -0500

    Fixing transactions and address conflict

--- resources/views/layouts/other/transactions.blade.php
+++ resources/views/layouts/other/transactions.blade.php
@@ -13,7 +13,7 @@
             <td><a href="/transaction/{{ $transaction->transaction_id }}">{{ $transaction->transaction_id }}</a></td>
             <td>{{ number_format($transaction->amount, 8) }}</td>
             <td>{{ $transaction->to_address }}</td>
-            <td>{{ \Carbon\Carbon::parse($address->created_at)->toDayDateTimeString() }} ({{ \Carbon\Carbon::parse($address->created_at)->diffForHumans() }})</td>
+            <td>{{ \Carbon\Carbon::parse($transaction->created_at)->toDayDateTimeString() }} ({{ \Carbon\Carbon::parse($address->created_at)->diffForHumans() }})</td>
         </tr>
     @endforeach
 </table>
