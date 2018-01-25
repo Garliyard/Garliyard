b4fe2fc24fcd549d1149b46d01ed627b8b247f02
Ely Haughie <elycin2@gmail.com>
2018-01-25 13:19:56 -0500

    fixing alert color

--- resources/views/dashboard/account/2fa/index.blade.php
+++ resources/views/dashboard/account/2fa/index.blade.php
@@ -33,7 +33,7 @@
                 @if(session()->has("success"))
                     <div class="row">
                         <div class="col-md-4 col-md-offset-4">
-                            <div class="alert alert-danger">
+                            <div class="alert alert-success">
                                 {{ session()->get("success") }}
                             </div>
                         </div>
