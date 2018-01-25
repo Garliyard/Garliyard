abd323e4d45321711a3fe06c405e0c58252e43f2
Ely Haughie <elycin2@gmail.com>
2018-01-25 08:18:45 -0500

    Adding list of things

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,11 +14,12 @@
     <div class="row">
         <div class="col-md-6">
             <h2 class="font-bold">Welcome to {{ config('app.name', 'Garlicoin') }}</h2>
-
-            <p>
-                This project is in a development state, and it should be used with caution.
-            </p>
-
+            <ul>
+                <li>No service fees</li>
+                <li>100 unique addresses</li>
+                <li>Balance confirmed after 3 confirmations</li>
+                <li>Instantaneous blockchain interaction (transactions are not delayed)</li>
+            </ul>
             <p>
                 If you need support, please contact <strong>{{ env('DISCORD_SUPPORT', 'Undefined#0000') }}</strong> on Discord.
             </p>
