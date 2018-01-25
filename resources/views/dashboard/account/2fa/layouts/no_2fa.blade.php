3c8512d4aa75daa31c1ede897a48e50bedf10189
Ely Haughie <elycin2@gmail.com>
2018-01-25 12:29:58 -0500

    Fixing spacing

--- resources/views/dashboard/account/2fa/layouts/no_2fa.blade.php
+++ resources/views/dashboard/account/2fa/layouts/no_2fa.blade.php
@@ -1,11 +1,11 @@
-<div class="col-md-6 col-md-offset-3">
+<div class="col-md-4 col-md-offset-4">
     <div class="panel panel-warning">
         <div class="panel-heading">
             YUBIKEYS
         </div>
         <div class="panel-body text-center">
             <span class="fa fa-key fa-3x"></span>
-            <br>
+            <br><br>
             <h3>No Yubikeys attached to your account</h3>
             <p>We're still working on Google Authenticator.</p>
             <br><br>
