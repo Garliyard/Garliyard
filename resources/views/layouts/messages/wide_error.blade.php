f00d102f684691632e4e2ef12e99a99ebd0c6d02
Ely Haughie <elycin2@gmail.com>
2018-01-25 13:34:23 -0500

    Adding more notification options

--- resources/views/layouts/messages/wide_error.blade.php
+++ resources/views/layouts/messages/wide_error.blade.php
@@ -7,3 +7,33 @@
         </div>
     </div>
 @endif
+
+@if(session()->has("warning"))
+    <div class="row">
+        <div class="col-md-12">
+            <div class="alert alert-warning">
+                {{ session()->get("warning") }}
+            </div>
+        </div>
+    </div>
+@endif
+
+@if(session()->has("success"))
+    <div class="row">
+        <div class="col-md-12">
+            <div class="alert alert-success">
+                {{ session()->get("success") }}
+            </div>
+        </div>
+    </div>
+@endif
+
+@if(session()->has("info"))
+    <div class="row">
+        <div class="col-md-12">
+            <div class="alert alert-info">
+                {{ session()->get("info") }}
+            </div>
+        </div>
+    </div>
+@endif
