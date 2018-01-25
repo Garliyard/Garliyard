13387b1ad7d91c042525ae0ae26ccc12e7d4198c
Ely Haughie <elycin2@gmail.com>
2018-01-25 09:52:10 -0500

    Removing dash for nbsp

--- resources/views/layouts/other/addresses.blade.php
+++ resources/views/layouts/other/addresses.blade.php
@@ -16,7 +16,7 @@
                 <td>{{ number_format(\App\Address::getReceived($address->address), 8) }}</td>
                 <td>
                     {{ $address->label or "No Label Present" }}
-                    - <a href="/edit-label/{{ $address->address }}"><span class="fa fa-pencil"></span></a>
+                    &nbsp; <a href="/edit-label/{{ $address->address }}"><span class="fa fa-pencil"></span></a>
                 </td>
                 <td>{{ \Carbon\Carbon::parse($address->created_at)->toDayDateTimeString() }} ({{ \Carbon\Carbon::parse($address->created_at)->diffForHumans() }})</td>
                 <td><a href="/qr-code/{{ $address->address }}" style="color: #000"><span class="fa fa-qrcode" title="Get QR Code"></span></a></td>
