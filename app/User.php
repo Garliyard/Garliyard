953fa8cd0019552e537cf857eaf86081b5c9012a
Ely Haughie <elycin2@gmail.com>
2018-01-26 21:38:26 -0500

    Preparing to add currency exchange rates with automatic guessing of country code and currency

--- app/User.php
+++ app/User.php
@@ -2,8 +2,8 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 
@@ -44,5 +44,42 @@ class User extends Authenticatable
     public static function welcome()
     {
         session()->flash("warning", sprintf("Welcome Back, %s! %s", Auth::user()->username, env("MOTD", "")));
+    }
+
+    private static $currency_location_array = [
+        "USD" => [
+            "US", "CA"
+        ],
+        "EUR" => [
+            "FR", "DE", "NO", "GB", "SE"
+        ]
+    ];
+
+    public static function getCurrency()
+    {
+        $user_country_code = self::getCountryCode();
+        foreach (self::$currency_location_array as $currency => $countries) {
+            foreach ($countries as $country) if ($country == $user_country_code) return $currency;
+        }
+        return "USD"; // Default Currency
+    }
+
+    public static function getCountryCode()
+    {
+        return (isset($_SERVER["HTTP_CF_IPCOUNTRY"])) ? $_SERVER["HTTP_CF_IPCOUNTRY"] : self::resolveCountryCodeFromIpAddress();
+    }
+
+    public static function resolveCountryCodeFromIpAddress()
+    {
+        $ip = self::getIPAddress();
+        return Cache::tags('country-code-resolver')->remember($ip, 86400, function () use ($ip) {
+            $resolver_data = json_decode(file_get_contents(sprintf("https://ipinfo.io/75.44.102.253/json", $ip)), true);
+            return (isset($resolver_data["country"])) ? $resolver_data["country"] : "US";
+        });
+    }
+
+    public static function getIPAddress()
+    {
+        return (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER["REMOTE_ADDR"];
     }
 }
