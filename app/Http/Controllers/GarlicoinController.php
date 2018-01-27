5d28c0210a28f2958fa78d7687b337c535ee5f1b
Ely Haughie <elycin2@gmail.com>
2018-01-27 12:47:40 -0500

    It's mentioned in the API that we use that there can be 10 requests per minute, so instead we'll just drop it down to a 1:1 ratio.

--- app/Http/Controllers/GarlicoinController.php
+++ app/Http/Controllers/GarlicoinController.php
@@ -327,7 +327,7 @@ class GarlicoinController extends JsonRpcController
 
     public function exchangeRate()
     {
-        return Cache::tags('exchange')->remember('usd', 3, function () {
+        return Cache::tags('exchange')->remember('usd', 1, function () {
             return json_decode(file_get_contents("https://api.coinmarketcap.com/v1/ticker/garlicoin/"), true)[0]["price_usd"];
         });
     }
